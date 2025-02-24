41c059455fcde3b6c845b1e0d20f51ac80bf78be
ayoub <ayoub.ouhen@gmail.com>
2025-02-24 16:11:18 +0100

    Créer l'interface IDao avec une méthode getData et une implémentation de cette interface

---
+++ src/main/java/dao/DaoImpl.java
@@ -0,0 +1,12 @@
+package dao;
+
+public class DaoImpl implements Idao{
+
+    @Override
+    public double getData() {
+        System.out.println("Version base de donnes ");
+        double temp = 10;
+
+        return temp;
+    }
+}
