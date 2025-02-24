efc561275d62b9cf2d7340b4be497ce8ed5e6f7a
ayoub <ayoub.ouhen@gmail.com>
2025-02-24 17:04:59 +0100

    l'injection des dépendances : Par instanciation dynamique via constructeur avec premier implementation(base de donnee)

---
+++ src/main/java/presentation/PresentationV2.java
@@ -0,0 +1,37 @@
+package presentation;
+
+import dao.Idao;
+import metier.IMetier;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class PresentationV2 {
+    public static void main(String[] args) {
+
+        try {
+
+            Scanner sc = new Scanner(new File("config.txt"));
+            String daoClasseName = sc.nextLine();
+
+            Class cdao = Class.forName(daoClasseName);
+            Idao idao = (Idao) cdao.getDeclaredConstructor().newInstance();
+
+            String metierClassName = sc.nextLine();
+
+            Class cmetier=Class.forName(metierClassName);
+            IMetier metier = (IMetier) cmetier.getDeclaredConstructor(Idao.class).newInstance(idao);
+
+            System.out.println(metier.calculer());
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+    }
+}
